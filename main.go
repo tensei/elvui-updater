39889e7c6a47a73f5d51b38a05f5130b28a7dfd6
Tensei <davuttensei@gmail.com>
2018-07-27 12:52:09 +0200

    goformat didn't format

--- main.go
+++ main.go
@@ -187,7 +187,7 @@ func install(settings *Settings, addons []ClientApiAddon) {
 		log.Println(err)
 		return
 	}
-	if _, ok:= inSlice(settings.Addons, a.Name); !ok {
+	if _, ok := inSlice(settings.Addons, a.Name); !ok {
 		settings.Addons = append(settings.Addons, a.Name)
 		settings.Save()
 	}
